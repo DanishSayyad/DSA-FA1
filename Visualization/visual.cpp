c51b4569076ee7401dcbc593a9199ff58e113f73
Danish Sayyad <danishsayyad2006@gmail.com>
2025-08-11 21:47:51 +0530

    Added sorting and array populator.

--- Visualization/visual.cpp
+++ Visualization/visual.cpp
@@ -1,15 +1,29 @@
 #include <SFML/Graphics.hpp>
 #include<iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace sf;
 using namespace std;
 
-RenderWindow window(VideoMode(512, 512), "Sorting Comparisions");
+void populateRandom(int[], int);
+int partition(int[], int, int);
+void quickSort(int[], int, int);
+
+RenderWindow window(VideoMode(1200, 800), "Sorting Visualizer");
 
 int main() {
     
-    Event event;
+    const int n = 200;
+    int arr1[n];
 
+    populateRandom(arr1, n);
+    int arr2[n];
+    for (int i = 0; i < n; ++i)
+        arr2[i] = arr1[i];
+    
+    Event event;
+    
     while(window.isOpen()) {
         while (window.pollEvent(event))
         {
@@ -19,4 +33,32 @@ int main() {
     }
 
     return 0;
+}
+
+void populateRandom(int arr[], int n) {
+    srand(static_cast<unsigned int>(time(0)));
+    for (int i = 0; i < n; ++i) {
+        arr[i] = rand() % (n + 1);
+    }
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
